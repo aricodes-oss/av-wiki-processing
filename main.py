b8931a5bfb0715c9b37929408556bff56e02ad48
Aria Taylor <ari@aricodes.net>
2026-05-16 15:52:01 -0700

    Load dump into memory

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import mwxml
+import argparse
+import glob
+import os
 
-def main():
-    print("Hello from av-wiki-process!")
+from mwxml import Dump
+
+
+def main(args):
+    file_path = args.file
+
+    if os.path.isdir(file_path):
+        xml_files = glob.glob(os.path.join(file_path, '*.xml'))
+        if len(xml_files) != 1:
+            raise ValueError(f"Expected 1 xml file, got {len(xml_files)}: {xml_files}")
+        file_path = xml_files[0]
+
+    dump = Dump.from_file(open(file_path))
+
+    # import bpdb
+    # bpdb.set_trace()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Path to the directory containing .xml file', default='wikidump')
+    args = parser.parse_args()
+
+    main(args)
